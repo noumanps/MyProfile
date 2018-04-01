e8688d276955ace28257795a5d75a366d72a6a6f
Nouman <khan.nomaan21@gmail.com>
2018-04-01 16:29:10 +0530

    Added navigation using OnClickListener

--- app/src/main/java/com/example/nouman/myprofile/MainActivity.kt
+++ app/src/main/java/com/example/nouman/myprofile/MainActivity.kt
@@ -4,27 +4,35 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var photo: ImageView? = null
+    var eduButton: Button? = null
+    var workButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        profilePhoto.setOnClickListener{
+        photo = findViewById(R.id.profilePhoto)
+        photo?.setOnClickListener{
             val intent = Intent(this, ProfilePhotoActivity::class.java)
             startActivity(intent)
         }
 
-        education.setOnClickListener {
+        eduButton = findViewById(R.id.education)
+        eduButton?.setOnClickListener {
             val intent = Intent(this, EducationActivity::class.java)
-            startActivity(intent);
+            startActivity(intent)
         }
 
-        experience.setOnClickListener{
+        workButton = findViewById(R.id.experience)
+        workButton?.setOnClickListener{
             val intent = Intent(this, ExperienceActivity::class.java)
-            startActivity(intent);
+            startActivity(intent)
         }
     }
 
